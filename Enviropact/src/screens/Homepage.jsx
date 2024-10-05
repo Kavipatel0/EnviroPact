cf6a53073e22add70302d974bc506ead7be038e8
Logan Bjork <loganbjork@ufl.edu>
2024-10-05 04:24:46 -0400

    homepage

--- Enviropact/src/screens/Homepage.jsx
+++ Enviropact/src/screens/Homepage.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import { Button } from "antd";
-import "./Homepage.css";
 
 function Homepage() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <ul><li>Enviro-Pact</li></ul>     
-      <Button type="primary" className="float-right" style={{ background:"#266347" }}>Sign Up</Button>     
-      <h1 className="text-red-50">Preserve your pact with the Earth.</h1>     
-      <div class="subheading">A new way to engage with your community and better the Earth together, one trash bag at a time.</div>
-      <Button type="primary">Search Events →</Button>
+      <div className="bg-green-950">
+        <nav className="p-10 h-8">
+          <ul>
+            <li className="float-left text-zinc-300">Enviro-Pact</li>
+            <li><Button type="primary" className="float-right text-black" style={{background:"rgb(132 204 22)"}}>Sign Up</Button></li>
+          </ul>     
+        </nav>
+
+        <body className="min-h-screen h-screen relative">
+          <div className="p-12">
+            <h1 className="text-zinc-300 font-bold text-5xl max-w-50">Preserve your pact with the Earth.</h1>     
+            <div className="text-lime-500 pb-5">A new way to engage with your community and better the Earth together, one trash bag at a time.</div>
+            <Button type="primary" className="text-black" style={{background:"rgb(132 204 22)"}}>Search Events →</Button>
+          </div>
+          <img src="../images/swoop-hands.svg" className="absolute top-10 bottom-0 left-0"></img>
+        </body>
+      </div>
     </>
   );
 }
