dede119d8839caf9a98651df330c0a84581eed0a
Stanley Ke <kestanley101@gmail.com>
2024-10-05 04:26:17 -0400

    update stanley

--- Enviropact/src/screens/Homepage.jsx
+++ Enviropact/src/screens/Homepage.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./Homepage.css";
+import { Button } from "antd";
 
 function Homepage() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-red-50">Preserve your pact with the Earth.</h1>
+      <h1 className="text-red-500">Preserve your pact with the Earth.</h1>
+      <Button className="text-orange-500">BRUH</Button>
     </>
   );
 }
