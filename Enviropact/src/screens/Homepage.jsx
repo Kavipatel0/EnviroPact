2a3adf3cd0473d3d6228f6889f0d7a03034d06f0
Logan Bjork <loganbjork@ufl.edu>
2024-10-05 02:52:20 -0400

    homepage

--- Enviropact/src/screens/Homepage.jsx
+++ Enviropact/src/screens/Homepage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "antd";
 import "./Homepage.css";
 
 function Homepage() {
@@ -6,7 +7,11 @@ function Homepage() {
 
   return (
     <>
-      <h1 className="text-red-50">Preserve your pact with the Earth.</h1>
+      <ul><li>Enviro-Pact</li></ul>     
+      <Button type="primary" className="float-right" style={{ background:"#266347" }}>Sign Up</Button>     
+      <h1 className="text-red-50">Preserve your pact with the Earth.</h1>     
+      <div class="subheading">A new way to engage with your community and better the Earth together, one trash bag at a time.</div>
+      <Button type="primary">Search Events →</Button>
     </>
   );
 }
