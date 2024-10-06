0b2b78bc60aaac1849a643c5ec94a720336ab3e8
Stanley Ke <kestanley101@gmail.com>
2024-10-05 20:36:50 -0400

    updates hero

--- Enviropact/src/screens/Homepage.jsx
+++ Enviropact/src/screens/Homepage.jsx
@@ -12,10 +12,10 @@ function Homepage() {
             <li>
               <img
                 src="../../assets/images/tree-icon.svg"
-                className="float-left px-5"
+                className="float-left px-2"
               ></img>
             </li>
-            <li className="py-4 float-left text-8-xl font-bold text-black geist-reg">
+            <li className="flex-row justify-center py-2 float-left text-lg text-black geist-reg">
               Enviro-Pact
             </li>
             <li>
@@ -33,7 +33,7 @@ function Homepage() {
         <body>
           {/*Hero Page */}
           <section className="min-h-screen bg-[url('../../assets/images/Background.svg')] bg-cover bg-no-repeat bg-right-bottom">
-            <div className="p-12 py-40 max-w-full">
+            <div className="px-20 py-40 max-w-full">
               <h1 className="font-bold text-6xl text-zinc-300 geist-reg">
                 Preserve your pact
                 <br />
