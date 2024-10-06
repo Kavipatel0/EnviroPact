77f487bc822420de3776717a90daa5b0276f0aff
Logan Bjork <loganbjork@ufl.edu>
2024-10-06 00:50:37 -0400

    completed cards and hyperlinks

--- Enviropact/src/screens/About.jsx
+++ Enviropact/src/screens/About.jsx
@@ -32,15 +32,66 @@ function About() {
             <div className="p-12 py-4 max-w-full">
                 <h1 className="font-bold text-center text-black text-6xl geist-reg">Meet the Team</h1>
                 <p className="p-8 text-2xl flex justify-center geist-reg">Team SLAK (this is placeholder text)</p>
-                <div className="p-10 flex flex-row justify-between items-center">
+                <div className="p-10 flex flex-row items-center justify-center space-x-32">
                     <Card style={{ width: 500 }}>
                         <img src="../../assets/images/adrian.jpg" className="object-cover w-full"></img>
                         <h2 className="pt-6 flex justify-center text-3xl font-bold geist-reg">Adrian Moreno</h2>
-                        <p className="p-2 flex justify-center text-lg geist-reg">Hello! My name is Adrian. Good one! Good one! Good one! Bad one.</p>
+                        <p className="p-2 flex justify-center text-lg geist-reg">Placeholder</p>
+                        <p className="pb-4 text-zinc-600 flex justify-center text-lg geist-reg">Backend</p>
+                        <div className="flex justify-center items-center">
+                            <a href="https://www.linkedin.com/in/adrian-moreno25/" className="flex items-center justify-center w-16">
+                                <img src="../../assets/images/LI-In-Bug.png" className="flex w-12"></img>
+                            </a>
+                            <a href="https://github.com/adriantoby" className="flex items-center justify-center w-12">
+                                <img src="../../assets/images/github-mark.svg" className="flex w-12"></img>
+                            </a>
+                        </div>
+                    </Card>
+
+                    <Card style={{ width: 500 }}>
+                        <img src="../../assets/images/kavi.jpg" className="object-cover w-full"></img>
+                        <h2 className="pt-6 flex justify-center text-3xl font-bold geist-reg">Kavi Patel</h2>
+                        <p className="p-2 flex justify-center text-lg geist-reg">Placeholder</p>
                         <p className="pb-4 text-zinc-600 flex justify-center text-lg geist-reg">Backend</p>
                         <div className="flex justify-center space-x-2">
-                            <img src="../../assets/images/LI-In-Bug.png" className="flex flex-row object-scale-down h-1/12 w-1/12"></img>
-                            <img src="../../assets/images/github-mark.svg" className="flex flex-row object-scale-down h-1/12 w-1/12" onClick={()=> handleClick}></img>
+                            <a href="https://www.linkedin.com/in/kavip05/" className="flex items-center justify-center w-16">
+                                <img src="../../assets/images/LI-In-Bug.png" className="flex w-12"></img>
+                            </a>
+                            <a href="https://github.com/Kavipatel0" className="flex items-center justify-center w-12">
+                                <img src="../../assets/images/github-mark.svg" className="flex w-12"></img>
+                            </a>
+                        </div>
+                    </Card>
+                </div>
+
+                <div className="p-10 flex flex-row items-center justify-center space-x-32">
+                    <Card style={{ width: 500 }}>
+                        <img src="../../assets/images/stanley.jpg" className="object-cover w-full"></img>
+                        <h2 className="pt-6 flex justify-center text-3xl font-bold geist-reg">Stanley Ke</h2>
+                        <p className="p-2 flex justify-center text-lg geist-reg">Placeholder</p>
+                        <p className="pb-4 text-zinc-600 flex justify-center text-lg geist-reg">Frontend</p>
+                        <div className="flex justify-center space-x-2">
+                            <a href="https://www.linkedin.com/in/stanley-ke/" className="flex items-center justify-center w-16">
+                                <img src="../../assets/images/LI-In-Bug.png" className="flex w-12"></img>
+                            </a>
+                            <a href="https://github.com/SobaSkee" className="flex items-center justify-center w-12">
+                                <img src="../../assets/images/github-mark.svg" className="flex w-12"></img>
+                            </a>
+                        </div>
+                    </Card>
+
+                    <Card style={{ width: 500 }}>
+                        <img src="../../assets/images/placeholder.png" className="object-cover w-full"></img>
+                        <h2 className="pt-6 flex justify-center text-3xl font-bold geist-reg">Logan Bjork</h2>
+                        <p className="p-2 flex justify-center text-lg geist-reg">Placeholder</p>
+                        <p className="pb-4 text-zinc-600 flex justify-center text-lg geist-reg">Frontend</p>
+                        <div className="flex justify-center space-x-2">
+                            <a href="https://www.linkedin.com/in/loganbjork/" className="flex items-center justify-center w-16">
+                                <img src="../../assets/images/LI-In-Bug.png" className="flex w-12"></img>
+                            </a>
+                            <a href="https://github.com/loganjaymes" className="flex items-center justify-center w-12">
+                                <img src="../../assets/images/github-mark.svg" className="flex w-12"></img>
+                            </a>
                         </div>
                     </Card>
                 </div>
