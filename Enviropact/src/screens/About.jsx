f274914a250862c9cb6e7cfef9ae66181a02bf79
Logan Bjork <loganbjork@ufl.edu>
2024-10-06 20:18:07 -0400

    qna info 2

--- Enviropact/src/screens/About.jsx
+++ Enviropact/src/screens/About.jsx
@@ -248,7 +248,7 @@ function About() {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1, ease: "easeIn"}}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow 
           </motion.p>
 
           <h2 className="p-4 text-center text-black text-4xl font-bold geist-reg">How It's Made</h2>
@@ -258,7 +258,7 @@ function About() {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1, ease: "easeIn"}}>
             For our frontend, we built Enviropact using React.js, Tailwind CSS, and the AntDesign and Framer Motion libraries.
-            
+
             For the backend, we utilized JavaScript, Firestore as a database, and Firebase as a BaaS and authenticator. 
           </motion.p>
 
